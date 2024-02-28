8935940e700e5186cd5735c9b7fcd4e036b3c736
Charanvardhan <charanvardhanreddym@gmail.com>
2024-02-27 20:46:21 -0500

    Revert "STS to DJ"
    This reverts commit 1f5ec1c8ce6f3992576b26b218af088cff943ac4.

---
+++ Backend/team4SEApp/src/main/java/Team4SE/team4SEApp/entity/Test.java
@@ -0,0 +1,43 @@
+package Team4SE.team4SEApp.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="test")
+public class Test {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column
+	private String name;
+}
