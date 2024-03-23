ff7c36ea5ee34d3e85d6b099794e43f46ef1e353
“aalexisadkins <ana04657@uga.edu>
2024-03-23 12:41:59 -0400

    started working on reset password
    didnt complete

--- TeamA4Django/TeamA4Django/settings.py
+++ TeamA4Django/TeamA4Django/settings.py
@@ -72,7 +72,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'TeamA4Django.wsgi.application'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 
 
 # Database
