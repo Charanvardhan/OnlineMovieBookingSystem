85088fdcf150f313e30ca5d518c76bc1780d2e83
megmgleason <marymargaretgleason@gmail.com>
2024-04-28 23:20:50 -0400

    Some routing fixes and ui changes to m_details

--- TeamA4Django/users/models.py
+++ TeamA4Django/users/models.py
@@ -47,6 +47,7 @@ class UserProfile(models.Model):
     billing_zip_postal_code = models.CharField(max_length=12, blank=True)
     subscribe_to_promotions = models.BooleanField(default=False)
 
+
     def __str__(self):
         return f"{self.user.username}'s profile"
     
